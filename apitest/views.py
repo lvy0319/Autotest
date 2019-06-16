7137c97aba63ebc2c7a6f723e566d35b305b2e3f
lvy0319 <lv5201@vip.qq.com>
2019-06-16 14:38:06 +0800

    1.增加登录退出 2.优化页面

--- apitest/views.py
+++ apitest/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 
@@ -7,4 +10,24 @@ def test(request):
     return HttpResponse('hello test!!!')
 
 def login(request):
+    if request.POST:
+        username = password = ''
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username,password=password)
+        if user is not None and user.is_active:
+            auth.login(request,user)
+            request.session['user'] = username
+            response = HttpResponseRedirect('/home/')
+            return response
+        else:
+            return render(request,'login.html',{'error':'username or password error!'})
     return render(request,'login.html')
+
+
+def home(request):
+    return render(request,"home.html")
+
+def logout(request):
+    auth.logout(request)
+    return render(request,"login.html")
